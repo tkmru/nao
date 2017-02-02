a19a4b696b173405cc4fe6514d4e8821215ef31c
tkmru <i.am.tkmru@gmail.com>
2017-02-02 16:31:29 +0900

    change dir for tests

---
+++ tests/test_eliminate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+# coding: UTF-8
+
+import unittest
+from nao import eliminate
+
+
+class TestEliminate(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
